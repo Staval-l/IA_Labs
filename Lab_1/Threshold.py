a86b1fb704afc97b59c7c41af923aa0923b4d7de
Staval <stalkervaultboy@gmail.com>
2024-02-18 22:35:10 +0400

    Почистил код

--- Lab_1/Threshold.py
+++ Lab_1/Threshold.py
@@ -1,10 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import skimage.data as data
-import skimage.segmentation as seg
-import skimage.filters as filters
-import skimage.draw as draw
-import skimage.color as color
 from skimage.io import imread, imsave
 from skimage import exposure
 from skimage.morphology import disk
@@ -27,13 +22,13 @@ ax.hist(img.ravel(), bins=256, range=[0, 256])
 ax.set_xlim(0, 256)
 plt.show()
 
-# p2, p98 = np.percentile(img, (2, 98))
-# img_rescale = exposure.rescale_intensity(img, in_range=(p2, p98))
-# image_show(img_rescale)
-# fig, ax = plt.subplots(1, 1)
-# ax.hist(img_rescale.ravel(), bins=256, range=[0, 256])
-# ax.set_xlim(0, 256)
-# plt.show()
+p2, p98 = np.percentile(img, (2, 98))
+img_rescale = exposure.rescale_intensity(img, in_range=(p2, p98))
+image_show(img_rescale)
+fig, ax = plt.subplots(1, 1)
+ax.hist(img_rescale.ravel(), bins=256, range=[0, 256])
+ax.set_xlim(0, 256)
+plt.show()
 
 selem = disk(100)
 img_eq = rank.equalize(img, footprint=selem)
@@ -44,18 +39,18 @@ ax.set_xlim(0, 256)
 plt.show()
 
 
-# img_eq = exposure.equalize_hist(img)
-# image_show(img_eq)
-# fig, ax = plt.subplots(1, 1)
-# ax.hist(img_eq.ravel(), bins=256, range=[0, 256])
-# ax.set_xlim(0, 256)
-# plt.show()
+img_eq = exposure.equalize_hist(img)
+image_show(img_eq)
+fig, ax = plt.subplots(1, 1)
+ax.hist(img_eq.ravel(), bins=256, range=[0, 256])
+ax.set_xlim(0, 256)
+plt.show()
 
 
-# text_segmented = img > 100
-# # text_segmented = filters.threshold_local(img, block_size=21, offset=10)
-# image_show(text_segmented);
-# fig, ax = plt.subplots(1, 1)
-# ax.hist(text_segmented.ravel(), bins=256, range=[0, 1])
-# ax.set_xlim(0, 1)
-# plt.show()
+text_segmented = img > 100
+# text_segmented = filters.threshold_local(img, block_size=21, offset=10)
+image_show(text_segmented);
+fig, ax = plt.subplots(1, 1)
+ax.hist(text_segmented.ravel(), bins=256, range=[0, 1])
+ax.set_xlim(0, 1)
+plt.show()
